2cdf15c896cb8fc2121fbef4c0499f92111a13b0
bn-salah05 <salah.benhari@gmail.com>
2024-09-11 21:53:08 +0100

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const Header = () => {
+  const cart = useSelector((state) => state.cart);
+  const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+
+  return (
+    <header>
+      <Link to="/products">Products</Link>
+      <Link to="/cart">Cart ({totalItems})</Link>
+    </header>
+  );
+};
+
+export default Header;
