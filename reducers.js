9e8c345b788ee166d7649a97e787a5b1a36fa422
bn-salah05 <salah.benhari@gmail.com>
2024-09-11 21:49:15 +0100

    Create reducers.js

---
+++ reducers.js
@@ -0,0 +1,51 @@
+import { ADD_TO_CART, INCREASE_QUANTITY, DECREASE_QUANTITY, REMOVE_FROM_CART } from './actions';
+
+const initialState = {
+  cart: [],
+};
+
+export const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const existingProduct = state.cart.find((item) => item.id === action.payload.id);
+      if (existingProduct) {
+        return {
+          ...state,
+          cart: state.cart.map((item) =>
+            item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
+          ),
+        };
+      }
+      return {
+        ...state,
+        cart: [...state.cart, { ...action.payload, quantity: 1 }],
+      };
+
+    case INCREASE_QUANTITY:
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload ? { ...item, quantity: item.quantity + 1 } : item
+        ),
+      };
+
+    case DECREASE_QUANTITY:
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload && item.quantity > 1
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        ),
+      };
+
+    case REMOVE_FROM_CART:
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+};
